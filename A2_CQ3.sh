bea05bd3c10b8952fbe3d2d3740907c34e9696f6
Aniket Dhiwar <aniketdhiwar1999@gmail.com>
2024-03-09 13:28:06 +0530

    file update

---
+++ A2_CQ3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Enter Number: "
+
+read num
+
+echo "Entered number is $num"
---
+++ A2_CQ4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter Number 1: "
+read num1
+echo "Enter Number 1: "
+read num2
+
+echo "Entered numbers are $num1 $num2"
+
+sum=$(( num1+num2 ))
+
+echo "Sum is $sum" 
